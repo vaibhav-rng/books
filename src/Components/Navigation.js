85c437670c5adbd4d3a803a2221de10ed95baa52
vaibhav kaushik <vaibhavkaushik1214@gmail.com>
2022-03-01 22:35:58 +0530

    image

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,7 +7,8 @@ function Navigation() {
             <span style={{backgroundColor:"#141E27",border:"solid #141E27"}} className="p-2 ">Home</span>
             <span> <a href="">Contact us</a> </span>
             <span><a href="">Catagories</a> </span>
-            <span className='float-right' > <input className=' outline-2 outline-yellow-100 p-1 -mt-1 text-center rounded-md ' type="search" name="" placeholder='Search book'/> </span>
+            <span className='float-right' > <input className=' outline-2 outline-yellow-100 p-1 -mt-1 text-center rounded-md ' type="search" name="" placeholder='Search book'/> <div className=" absolute top-1" > <img src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"/></div> </span>
+            
         </nav>
     </div>
   )
